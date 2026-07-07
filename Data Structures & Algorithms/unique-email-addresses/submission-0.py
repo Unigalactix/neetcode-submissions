91de6555848a69ea2dde4446608667e1481a0346
Rajesh Kodaganti (Rugved) <kodagantir295@gmail.com>
2026-07-07 12:28:53 -0700

    Add: unique-email-addresses - submission-0

---
+++ Data Structures & Algorithms/unique-email-addresses/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        unique_emails=set()
+        for email in emails:
+            local,domain=email.split('@')
+            local=local.split('+')[0]
+            local = local.replace('.','')
+            clean_email=local+'@'+domain
+            unique_emails.add(clean_email)
+        return len(unique_emails)
